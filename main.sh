4ea2638fb61afa6e614b477beb99ca4be7cba36b
huang chenghao <charryhuang@tencent.com>
2019-11-19 20:33:59 +0800

    feat: update.

--- main.sh
+++ main.sh
@@ -15,7 +15,7 @@ Usage: g <command> <...?>
   - :  git checkout -, 回到上一个分支
   ps:  git push origin [current branch], 推送代码到当前远程分支
   pl:  git pull origin <branchName>?, 从远程分支拉取代码
-  fh:  git fetch, 获取远程仓库变动
+  fc:  git fetch, 获取远程仓库变动
   cm:  <comment?|cmd?>, git commit <-m comment?|cmd?>
   sm:  <comment>?, g ad && g cm && g ps, 一键推送所有修改
   mg:  git merge <branchName?>, 拉取并 merge 目标分支, 默认 merge master
@@ -55,7 +55,7 @@ ps)
 pl)
   pull "$1"
   ;;
-fh)
+fc)
   git fetch
   ;;
 cm)
