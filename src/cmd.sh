e3d66934d54c74fab389fc07e247d6e6e33cbaac
huang chenghao <charryhuang@tencent.com>
2019-11-21 11:50:46 +0800

    feat: update.

--- src/cmd.sh
+++ src/cmd.sh
@@ -49,7 +49,7 @@ merge() {
 
   doMerge() {
     if [ -z "$targetBranch" ]; then
-      echo "$(git checkout master && git pull && git checkout - && git merge master)"
+      git checkout master && git pull && git checkout - && git merge master
     else
       git merge "$targetBranch"
     fi
